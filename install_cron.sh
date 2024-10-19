bf731c4712be8abd6e14c994b6f73efd11a6848b
lukasreinert <89549256+lukasreinert@users.noreply.github.com>
2024-10-19 12:52:51 +0200

    Update install cron and readme

--- install_cron.sh
+++ install_cron.sh
@@ -8,9 +8,9 @@ read -p "[Cron] Do you wish to install a cronjob that runs every 15 minutes? [Y/
 
 if [ "$install_auto_update" == "" ] || [ "$install_auto_update" == "y" ] || [ "$install_auto_update" == "Y" ]
 then
-echo "[Cron] Installing cronjob"
-(crontab -l; echo ""; echo "# cakecms"; echo "*/15 8-18 * * 1-5 python3 /root/cakecms/cakecms.py"; echo "") | crontab -
-echo "[Cron] Done"
+    echo "[Cron] Installing cronjob"
+    (crontab -l; echo ""; echo "# cakecms"; echo "*/15 8-18 * * 1-5 python3 $folder_path/cakecms.py"; echo "") | crontab -
+    echo "[Cron] Done"
 else
     echo "[Cron] Aborted"
 fi
